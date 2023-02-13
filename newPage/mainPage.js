a3b5f92777ff897f1f0167e6c584f8024e1c2467
polidor01 <polinadoratis@gmail.com>
2023-02-13 21:23:22 +0000

    6.1

---
+++ newPage/mainPage.js
@@ -0,0 +1,67 @@
+
+/*  the folowing lines were adapted from https://www.youtube.com/watch?v=mrVkYzMX%20s1s */
+
+function changeColor(obj) {
+
+    if (obj.style.color == "lightblue") {
+        obj.style.color = "black";
+    } else {
+        obj.style.color = "lightblue";
+    }
+}
+
+function showText() {
+    let element = document.getElementById("FAQ");
+
+    if (element.style.display == "none") {
+        element.style.display = "block";
+
+    } else {
+        element.style.display = "none";
+    }
+}
+
+/* the following lines were adapted from https://www.google.com/search?q=easy+animatio+to+do+in+javascript&oq=easy+animatio+to+do+in+javascript&aqs=chrome..69i57j33i10i22i29i30i625.10923j0j15&sourceid=chrome&ie=UTF-8#fpstate=ive&vld=cid:c6759f18,vid:hRutYc4UirU */
+
+
+function fadein()
+{
+    document.getElementById("img").style.opacity = 0.1;
+}
+
+function fadeout()
+{
+    document.getElementById("img").style.opacity = 1;
+}
+ 
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
