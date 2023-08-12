f1b1f7d75a80b9be088b47ad89f95d1f7bae9770
diana07042003 <117115007+diana07042003@users.noreply.github.com>
2023-08-12 15:46:40 +0300

    Add files via upload

---
+++ exam/main.js
@@ -0,0 +1,23 @@
+const card=document.querySelector('.card__inner');
+
+card.addEventListener('click', function(){
+    card.classList.toggle('is-flipped');
+});
+
+const card2=document.querySelector('.card__inner2');
+
+card2.addEventListener('click', function(){
+    card2.classList.toggle('is-flipped');
+});
+
+const card3=document.querySelector('.card__inner3');
+
+card3.addEventListener('click', function(){
+    card3.classList.toggle('is-flipped');
+});
+
+const card4=document.querySelector('.card__inner4');
+
+card4.addEventListener('click', function(){
+    card4.classList.toggle('is-flipped');
+});
